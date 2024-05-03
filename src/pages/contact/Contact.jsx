bbf1f92a1273e8bff300da8f600ff395ec671229
Sourav Prajapati <ksourav044@gmail.com>
2024-05-03 19:06:50 +0530

    .

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -24,6 +24,7 @@ const Contact = () => {
       .then(
         () => {
           console.log("SUCCESS!");
+          form.current.reset();
         },
         (error) => {
           console.log("FAILED...", error.text);
